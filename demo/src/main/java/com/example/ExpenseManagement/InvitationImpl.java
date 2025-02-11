24331b23fe66fff6273bc7510fc9c03563db26c5
Nandkishor <pagememe0110@gmail.com>
2025-02-11 11:25:03 +0530

    added exception handeler invitationService and repository

--- demo/src/main/java/com/example/ExpenseManagement/InvitationImpl.java
+++ demo/src/main/java/com/example/ExpenseManagement/InvitationImpl.java
@@ -1,7 +1,13 @@
 package com.example.ExpenseManagement;
 
 import com.example.ExpenseManagement.DTO.InvitationDTO;
+import com.example.ExpenseManagement.entities.Invitation;
+
+import java.util.List;
 
 public interface InvitationImpl {
-    String sendInvitation(String token,  InvitationDTO invitationDTO);
+    Invitation sendInvitation(String token, InvitationDTO invitationDTO);
+    List<Invitation> getAllInvitation();
+
+
 }
