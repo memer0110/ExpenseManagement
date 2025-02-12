fe29761eb04666472318fff0403d48018709f2a6
Aaryan1703 <aryanphadtare007@gmail.com>
2025-02-12 13:05:20 +0530

    deleted global exception file

--- demo/src/main/java/com/example/ExpenseManagement/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ExpenseManagement.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
